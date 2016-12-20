71205c5b1d2a500eff781b2c3318b153f1ac961b
LouisGerard <louis.gerardd@gmail.com>
2016-12-20 16:44:47 +0100

    config dialog (grid size)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QInputDialog>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+    this->gridSize = QInputDialog::getInt(this, "Taille de la grille", "Entrez une taille : ", 5, 4, 10);
+    qDebug() << "Grid size : " << gridSize;
     ui->setupUi(this);
+    this->show();
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -11,12 +11,14 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
+    //todo Singleton
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
 private:
     Ui::MainWindow *ui;
+    int gridsize;
 };
 
 #endif // MAINWINDOW_H
