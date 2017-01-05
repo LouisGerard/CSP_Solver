b30173b97bdb5d1cad1e7baf787fe9c11da84970
LouisGerard <louis.gerardd@gmail.com>
2017-01-05 13:44:22 +0100

    Patron de backtrack - changer la stack en un struct ? -> voir goto pour résoudre le problème de break

--- ai.cpp
+++ ai.cpp
@@ -15,8 +15,27 @@ AI::AI(Grid * base, std::vector<Constraint*> constraints, Heuristic *heuristic) 
 
 int* AI::BackTrackC()
 {
-    int sizeOfGrid = sizeof(int)*base->size()*base->size();
+    unsigned gridSize = base->size();
     int* grid = base->toC();
+
+    int varStack[gridSize*gridSize];
+    int stackTop = -1;
+    for (int* item = grid; item-grid < gridSize; ++i)
+        if (*item != 0)
+            varStack[++stackTop] = item-grid;
+
+    unsigned nbVars = stackTop + 1;
+
+    while (stackTop != -1) {
+        unsigned currentItem = varStack[stackTop];
+        //domain
+        for (unsigned i = 0; i < gridSize; ++i) {
+            *(grid+currentItem) = i;
+            if (isConsistantC(grid))
+                ;//--stackTop & break while
+        }
+        //++stackTop
+    }
 
     delete grid;
 }
