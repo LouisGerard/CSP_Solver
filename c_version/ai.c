8faa3c846ef8f44bdf621dddc2945a1efa2f365f
LouisGERARD <louis.gerardd@gmail.com>
2017-04-29 10:10:31 +0200

    avancées mis en commentaire

--- c_version/ai.c
+++ c_version/ai.c
@@ -279,53 +279,53 @@ bool forward_checking_optimized()
     }
 
     //evidences
-    int evidences[grid_size*grid_size];
+    /*int evidences[grid_size*grid_size];
     unsigned evidences_size = 0;
-    bool is_evident[grid_size*grid_size];
+    bool is_evident[grid_size*grid_size];*/
 
     //todo find name (ideas : sorter)
-    unsigned aze[5];
-    unsigned aze_size = 0;
+    unsigned sorter[5];
+    unsigned sorter_size = 0;
 
     //vars
     int varStack[grid_size*grid_size];
     int stackTop = -1;
     for (int* item = grid; item-grid < grid_size*grid_size; ++item) {
-        is_evident[item-grid] = false;
+        //is_evident[item-grid] = false;
         if (*item == 0) {
             ++stackTop;
 
             //sort
             bool index_assigned = false;
-            unsigned aze_index;
-            for (unsigned i = 0; i < aze_size; ++i) {
+            unsigned stack_index;
+            for (unsigned i = 0; i < sorter_size; ++i) {
                 if (index_assigned) {
-                    ++aze[i];
+                    ++sorter[i];
                     continue;
                 }
-                unsigned stack_pos = aze[i];
+                unsigned stack_pos = sorter[i];
                 if (nb_constraints[item-grid] <= nb_constraints[varStack[stack_pos]]) {
-                    aze_index = stack_pos;
-                    if (nb_constraints[item-grid] != nb_constraints[varStack[aze[i]]]) {
-                        unsigned temp[aze_size-i];
-                        memcpy(&temp[0], &aze[i], (aze_size-i)*sizeof(unsigned));
-                        memcpy(&aze[i+1], &temp[0], (aze_size-i)*sizeof(unsigned));
-                        ++aze_size;
+                    stack_index = stack_pos;
+                    if (nb_constraints[item-grid] != nb_constraints[varStack[sorter[i]]]) {
+                        unsigned temp[sorter_size-i];
+                        memcpy(&temp[0], &sorter[i], (sorter_size-i)*sizeof(unsigned));
+                        memcpy(&sorter[i+1], &temp[0], (sorter_size-i)*sizeof(unsigned));
+                        ++sorter_size;
                     }
                     index_assigned = true;
                 }
             }
             if (!index_assigned) {
-                aze_index = stackTop;
-                aze[aze_size++] = stackTop;
+                stack_index = stackTop;
+                sorter[sorter_size++] = stackTop;
             }
             else {
-                int temp[stackTop-aze_index];
-                memcpy(&temp[0], &varStack[aze_index], (stackTop-aze_index)*sizeof(int));
-                memcpy(&varStack[aze_index+1], &temp[0], (stackTop-aze_index)*sizeof(int));
+                int temp[stackTop-stack_index];
+                memcpy(&temp[0], &varStack[stack_index], (stackTop-stack_index)*sizeof(int));
+                memcpy(&varStack[stack_index+1], &temp[0], (stackTop-stack_index)*sizeof(int));
             }
 
-            varStack[aze_index] = item-grid;
+            varStack[stack_index] = item-grid;
         }
     }
 
@@ -345,7 +345,7 @@ bool forward_checking_optimized()
 
     for (int i = 0; i < grid_size*grid_size; ++i)
         if (*(grid+i) != 0)
-            filter_domains_with_evidences(i, domains, domainSizes[stackTop + 1], evidences, &evidences_size, is_evident);
+            filter_domains(i, domains, domainSizes[stackTop + 1]/*, evidences, &evidences_size, is_evident*/);
 
     while (true) {
         continue_while:
@@ -364,28 +364,20 @@ bool forward_checking_optimized()
         }
 
         ++iterations_cpt;
-        unsigned currentItem = varStack[stackTop];
-
-//
-        if (evidences_size > 0) {
-            show_grid();
-            printf("Domains : \n");
-            for (unsigned i = 0; i < grid_size * grid_size; ++i) {
-                printf("%d: ", i);
-                for (unsigned d = 0; d < grid_size; ++d) {
-                    printf("%d,", domains[i][d]);
-                    if (d == domainSizes[stackTop + 1][i] - 1)
-                        printf("|");
-                }
-                printf("\n");
-            }
-            printf("Evidences : ");
-            for (unsigned i = 0; i < evidences_size; ++i) {
-                printf("%d,", evidences[i]);
-            }
-            printf("\n");
-            printf("\n");
+        unsigned currentItem;
+        /*if (evidences_size > 0) {
+            currentItem = evidences[--evidences_size];
         }
+        else {
+            while(true) {*/
+                currentItem = varStack[stackTop];
+                /*if (!is_evident[currentItem])
+                    break;
+                if (--stackTop == -1)
+                    goto continue_while;
+                memcpy(&domainSizes[stackTop], &domainSizes[stackTop+1], grid_size*grid_size*sizeof(int));
+            }
+        }*/
 
         //eval domain
         for (; domainsOffsets[stackTop] < domainSizes[stackTop][currentItem]; ++domainsOffsets[stackTop]) {
@@ -393,34 +385,98 @@ bool forward_checking_optimized()
 
             memcpy(&domainSizes[stackTop], &domainSizes[stackTop+1], grid_size*grid_size*sizeof(int));
 
-            if (filter_domains_with_evidences(currentItem,
-                                domains,
-                                domainSizes[stackTop],
-                                evidences,
-                                &evidences_size,
-                                is_evident)) {
-                --stackTop;
+            if (filter_domains(currentItem,
+                               domains,
+                               domainSizes[stackTop]/*,
+                               evidences,
+                               &evidences_size,
+                               is_evident*/)) {
+                //if (!is_evident[currentItem])
+                    --stackTop;
+//
+                /*else {
+                    for (unsigned y = 0; y < grid_size; ++y) {
+                        for (unsigned x = 0; x < grid_size; ++x) {
+                            if (y * grid_size + x == currentItem)
+                                printf("|");
+                            else
+                                printf(" ");
+                            printf("%d", grid[y * grid_size + x]);
+                        }
+                        printf("\n");
+                    }
+                    printf("Domains : \n");
+                    for (unsigned i = 0; i < grid_size * grid_size; ++i) {
+                        printf("%d: ", i);
+                        for (unsigned d = 0; d < grid_size; ++d) {
+                            printf("%d,", domains[i][d]);
+                            if (d == domainSizes[stackTop][i] - 1)
+                                printf("|");
+                        }
+                        printf(" -> %s", (is_evident[i] ? "e" : "ne"));
+                        printf("\n");
+                    }
+                    printf("Evidences : ");
+                    for (unsigned i = 0; i < evidences_size; ++i) {
+                        printf("%d,", evidences[i]);
+                    }
+                    printf("\n");
+                    printf("\n");
+                }*/
+
                 goto continue_while;
             }
         }
 
-        //reinit domain
-        domainsOffsets[stackTop] = 0;
+        /*do {
+            currentItem = varStack[stackTop];
 
-        //reinit grid
-        *(grid+currentItem) = 0;
+            //todo reinit evidences
+            for (unsigned c = 0; c < constraints_size; ++c) {
+                for (unsigned a = 0; a < constraints[c]->size; ++a) {
+                    unsigned x1 = *(constraints[c]->array + a * 4);
+                    unsigned y1 = *(constraints[c]->array + a * 4 + 1);
+                    unsigned x2 = *(constraints[c]->array + a * 4 + 2);
+                    unsigned y2 = *(constraints[c]->array + a * 4 + 3);
+                    int item1 = y1 * grid_size + x1;
+                    int item2 = y2 * grid_size + x2;
+                    int evidence;
 
-        ++stackTop;
+                    if (currentItem != item1) {
+                        if (currentItem != item2)
+                            continue;
+                        evidence = item1;
+                    }
+                    else {
+                        evidence = item2;
+                    }
+                    if (!is_evident[evidence])
+                        continue;
+
+                    is_evident[evidence] = false;
+                    grid[evidence] = 0;
+                }
+            }*/
+
+            //reinit domain
+            domainsOffsets[stackTop] = 0;
+
+            //reinit grid
+            *(grid+currentItem) = 0;
+
+            ++stackTop;
+        //} while (is_evident[currentItem]);
+
         ++domainsOffsets[stackTop];
     }
 }
 
 bool filter_domains_with_evidences(int item,
-                    int ** domains,
-                    int * domSizes,
-                    int *evidences,
-                    unsigned *evidences_size,
-                    bool *is_evident)
+                                   int ** domains,
+                                   int * domSizes,
+                                   int *evidences,
+                                   unsigned *evidences_size,
+                                   bool *is_evident)
 {
     int *linkedVal;
     int evidences_cpy[grid_size*grid_size];
@@ -478,5 +534,6 @@ bool filter_domains_with_evidences(int item,
     }
     memcpy(&evidences[0], &evidences_cpy[0], evidences_cpy_size*sizeof(int));
     *evidences_size = evidences_cpy_size;
+
     return true;
 }
